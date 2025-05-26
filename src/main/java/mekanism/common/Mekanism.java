c38b1b5704d194205fde7b923fa312d74e773731
Sara Freimer <sara@freimer.com>
2025-05-26 16:41:54 -0500

    Bump min neo version to 21.1.171 and fix BlockEnergyCapabilityCache holding onto the capability for too long, and mark a couple of our handler caps as proxyable (#8411)

--- src/main/java/mekanism/common/Mekanism.java
+++ src/main/java/mekanism/common/Mekanism.java
@@ -204,6 +204,7 @@ public class Mekanism {
         NeoForge.EVENT_BUS.addListener(this::onDataMapsUpdated);
         NeoForge.EVENT_BUS.addListener(MekanismPermissions::registerPermissionNodes);
         NeoForge.EVENT_BUS.register(IncompleteRecipeScanner.class);
+        modEventBus.addListener(EventPriority.HIGH, Capabilities::registerProxyableCapabilities);
         modEventBus.addListener(Capabilities::registerCapabilities);
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::registerChunkTicketControllers);
